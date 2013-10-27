83a7775e9a67b5eefe31db9547ee434d14988d47
nuptboyzhb <hbzheng0618@gmail.com>
2013-10-27 21:51:52 +0800

    ServiceImplements

--- src/edu/njupt/zhb/service/DataGridDemoService.java
+++ src/edu/njupt/zhb/service/DataGridDemoService.java
@@ -20,73 +20,9 @@ import edu.njupt.zhb.tools.Tips;
  *mail:    zhb931706659@126.com
  *2013-10-26  Nanjing,njupt,China
  */
-public class DataGridDemoService {
-	private BaseDao<Student> baseDao;
-
-	public BaseDao<Student> getBaseDao() {
-		return baseDao;
-	}
-	public void setBaseDao(BaseDao<Student> baseDao) {
-		this.baseDao = baseDao;
-	}
-	public String getStudentList(int page, int rows) {
-		// TODO Auto-generated method stub
-		System.out.println("page="+page+",rows="+rows);
-		String hql = "from Student";
-		try {
-			List<Student> list = baseDao.find(hql,page,rows);
-			DataGrid<Student> dataGrid = new DataGrid<Student>();
-			dataGrid.setRows(list);
-			dataGrid.setTotal(baseDao.total(hql));
-			String result = JSONObject.fromObject(dataGrid).toString();
-			return result;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	public String addStudent(Student student) {
-		// TODO Auto-generated method stub
-		Tips tips = new Tips();
-		try {
-			baseDao.save(student);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			tips.setMsg("添加失败");
-			return JSONObject.fromObject(tips).toString();
-		}
-		tips.setMsg("添加成功");
-		return JSONObject.fromObject(tips).toString();
-	}
-	public String deleteStudent(int studentid) {
-		// TODO Auto-generated method stub
-		Tips tips = new Tips();
-		try {
-			baseDao.executeHql("delete from Student where id = "+studentid);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			tips.setMsg("删除失败");
-			return JSONObject.fromObject(tips).toString();
-		}
-		tips.setMsg("删除成功");
-		return JSONObject.fromObject(tips).toString();
-	}
-	public String editStudent(Student student) {
-		// TODO Auto-generated method stub
-		Tips tips = new Tips();
-		try {
-			baseDao.update(student);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			tips.setMsg("编辑失败");
-			return JSONObject.fromObject(tips).toString();
-		}
-		tips.setMsg("编辑成功");
-		return JSONObject.fromObject(tips).toString();
-	}
-
-
+public interface DataGridDemoService {
+	public String getStudentList(int page, int rows);
+	public String addStudent(Student student);
+	public String deleteStudent(int studentid);
+	public String editStudent(Student student);
 }
